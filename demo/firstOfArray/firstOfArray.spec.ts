3f60573c0750f0e19458ac52007e98f9f3e1566a
panther <22311632+lil-nail@users.noreply.github.com>
2023-07-17 12:06:17 +0000

    🧪 test(first of array): fix first of array test demo

--- demo/firstOfArray/firstOfArray.spec.ts
+++ demo/firstOfArray/firstOfArray.spec.ts
@@ -1,7 +1,7 @@
-import { describe } from 'vitest'
+import { test, expect } from 'vitest'
 import { First, First2 } from './firstOfArray'
 
-describe('first of array', () => {
+test('first of array', () => {
   type arr1 = ['a', 'b', 'c']
   type arr2 = [3, 2, 1]
 
@@ -11,8 +11,17 @@ describe('first of array', () => {
   type head1V2 = First2<arr1> // expected to be 'a'
   type head2V2 = First2<arr2> // expected to be 3
 
+  // First
   const result1: head1 = 'a'
   const result2: head2 = 3
+
+  expect(result1, 'a')
+  expect(result2, '3')
+
+  // First2
   const result1V2: head1V2 = 'a'
   const result2V2: head2V2 = 3
+
+  expect(result1V2, 'a')
+  expect(result2V2, '3')
 })
